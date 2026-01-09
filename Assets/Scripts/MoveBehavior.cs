245411da342b3d678f7dd1cf5cd5dbf4403fc74f
SergioV <sergio.vegas.7e8@itb.cat>
2026-01-09 17:44:40 +0100

    Movimienot refinado

--- Assets/Scripts/MoveBehavior.cs
+++ Assets/Scripts/MoveBehavior.cs
@@ -5,6 +5,7 @@ public class MoveBehavior : MonoBehaviour
 {
     private CharacterController _controller;
     public Vector3 CurrentVelocity { get; private set; }
+    public float rotateSpeed = 5f;
     private void Awake()
     { 
         _controller = GetComponent<CharacterController>();
@@ -12,15 +13,20 @@ public class MoveBehavior : MonoBehaviour
     public void MoveCharacter(Vector3 direction, float speed)
     {
         Vector3 move = new Vector3(direction.x, 0f, direction.z); 
-        _controller.Move(move * speed * Time.deltaTime);
+        if(move.z >0)
+            _controller.Move( transform.forward * move.z * speed * Time.deltaTime );
+        //_controller.Move(move * speed * Time.deltaTime);
         CurrentVelocity = move * speed;
     }
     public void RotateCharacter(Vector3 moveDir)
     {
         if (moveDir.sqrMagnitude > 0.01f) 
-        { 
-            Quaternion targetRotation = Quaternion.LookRotation(moveDir);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 10f * Time.deltaTime);
+        {
+            //float angle = Mathf.Atan2(moveDir.x, moveDir.z) * Mathf.Rad2Deg;
+            float angle = moveDir.x;
+            transform.Rotate(new Vector3(0, angle * Time.deltaTime * rotateSpeed, 0));
+            //Quaternion targetRotation = Quaternion.Euler(0,angle,0);
+            //transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 10f * Time.deltaTime);
         }
     }
 }
